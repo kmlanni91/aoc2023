8a8c121830d15d732a9e04f5d0338ab8f8ccfb00
Kevin Lanni <kmlanni91@gmail.com>
2024-01-21 17:40:02 -0500

    Day 6 part 1

---
+++ app/Day6.hs
@@ -0,0 +1,30 @@
+module Main where
+
+
+
+parseLine :: String -> [Int]
+parseLine = map read . tail . words
+
+
+calculateDistance :: Int -> Int -> Int
+calculateDistance totalTime holdTime = distance
+    where
+        travelTime = totalTime - holdTime
+        speed = holdTime
+        distance = speed * travelTime
+
+
+genDistances :: Int -> [Int]
+genDistances totalTime = map (calculateDistance totalTime) [0..totalTime]
+
+
+genRecordBeaters :: Int -> Int -> [Int]
+genRecordBeaters totalTime record = filter (>record) $ genDistances totalTime
+
+
+main :: IO ()
+main = do
+    input <- readFile "data/day6/input.txt"
+    let [times, distances] = map parseLine $ lines input
+    let recordBeaters = zipWith genRecordBeaters times distances
+    print $ product $ map length recordBeaters
